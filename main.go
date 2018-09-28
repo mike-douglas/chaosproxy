31f3635f408356a4e0c6724d3812fcee31ba3211
Mike Douglas <michaeldouglas@invisionapp.com>
2018-09-27 21:04:09 -0400

    Failed attempt at SSL support

--- main.go
+++ main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/mike-douglas/chaosproxy/config"
 	"github.com/mike-douglas/chaosproxy/structs"
 )
+
+func handleTunneling(w http.ResponseWriter, r *http.Request) {
+	dest, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	hijacker, ok := w.(http.Hijacker)
+
+	if !ok {
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+
+	client, _, err := hijacker.Hijack()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+
+	go transfer(dest, client)
+	go transfer(client, dest)
+}
+
+func transfer(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
+	io.Copy(destination, source)
+}
 
 func buildHandlerFromConfig(config *structs.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +74,9 @@ func buildHandlerFromConfig(config *structs.Config) http.HandlerFunc {
 
 func main() {
 	f := flag.String("config", "", "Configuration YAML file for proxy")
+	p := flag.Int("port", 8080, "The port to listen on")
+	pem := flag.String("pem", "", "SSL Cert")
+	key := flag.String("key", "", "SSL Key")
 
 	flag.Parse()
 
@@ -58,13 +97,32 @@ func main() {
 	}
 
 	r := mux.NewRouter()
+	h := buildHandlerFromConfig(&c)
 
-	r.HandleFunc("/", buildHandlerFromConfig(&c)).Methods("POST", "GET", "PUT", "PATCH", "DELETE")
+	r.HandleFunc("/", h).Methods("POST", "GET", "PUT", "PATCH", "DELETE", "CONNECT")
 
 	if c.Verbose {
 		fmt.Println(c)
 	}
 
-	fmt.Printf("Listening on %s\n", ":8080")
-	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, r))
+	fmt.Printf("Listening on %s\n", fmt.Sprintf(":%v", *p))
+	fmt.Println(*pem)
+
+	if len(*pem) > 0 && len(*key) > 0 {
+		fmt.Println("Starting SSL Server")
+		server := &http.Server{
+			Addr: fmt.Sprintf(":%v", *p),
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.Method == http.MethodConnect {
+					handleTunneling(w, r)
+				} else {
+					h(w, r)
+				}
+			}),
+			TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+		}
+		server.ListenAndServeTLS(*pem, *key)
+	} else {
+		http.ListenAndServe(fmt.Sprintf(":%v", *p), handlers.LoggingHandler(os.Stdout, r))
+	}
 }
